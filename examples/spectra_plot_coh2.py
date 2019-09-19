5db4dcb1d3119fbdfa65b03105f23b1e3c16a6f4
mgehne <mgehne@gmail.com>
2019-09-19 10:12:04 -0600

    changed paths to data and plots

--- examples/spectra_plot_coh2.py
+++ examples/spectra_plot_coh2.py
@@ -1,12 +1,11 @@
-import Ngl as ngl
 import numpy as np
-from tropical_diagnostics.diagnostics import spacetime_plot as stp
-import string
+from diagnostics import spacetime_plot as stp
+import xarray
 
 from netCDF4 import Dataset
 
-pathdata = './data/'
-plotpath = './plots/'
+pathdata = '../data/'
+plotpath = '../plots/'
 
 # plot layout parameters
 flim = 0.5            # maximum frequency in cpd for plotting
