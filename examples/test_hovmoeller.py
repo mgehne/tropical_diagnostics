85a589c08a47a42cbcdb3eaedb5e2c124d56b214
mgehne <mgehne@gmail.com>
2020-03-04 12:39:00 -0700

    edits to test_hovmoeller

--- examples/test_hovmoeller.py
+++ examples/test_hovmoeller.py
@@ -7,7 +7,6 @@ sys.path.append('../../')
 local scripts, if loading from a different directory include that with a '.' between
 directory name and script name
 """
-from tropical_diagnostics.diagnostics.hovmoeller_plotly import hovmoeller
 from tropical_diagnostics.diagnostics.hovmoeller_calc import lat_avg
 from tropical_diagnostics.diagnostics.hovmoeller_calc import pattern_corr
 
@@ -18,17 +17,11 @@ Parameters to set for the Hovmoeller diagrams.
 """
 datestrt = '2016-01-01'  # plot start date, format: yyyy-mm-dd
 datelast = '2016-03-31'  # plot end date, format: yyyy-mm-dd
-#contourmin = 0.2  # contour minimum
-#contourmax = 1.2  # contour maximum
-#contourspace = 0.2  # contour spacing
 latMax = 5.  # maximum latitude for the average
 latMin = -5.  # minimum latitude for the average
 
 print("reading data from file:")
-spd = 2  # number of obs per day
-source = "ERAI"  # data source
-var = "precip"  # variable to plot
-lev = ""   # level
+
 ds = xr.open_dataset('/data/mgehne/ERAI/MetricsObs/precip.erai.sfc.1p5.2x.1979-2016.nc')
 A = ds.precip
 lonA = ds.lon
@@ -43,12 +36,17 @@ A.attrs['units'] = 'mm/day'
 A = lat_avg(A, latmin=latMin, latmax=latMax)
 
 # print("plot hovmoeller diagram:")
+# spd = 2  # number of obs per day
+# source = "ERAI"  # data source
+# var = "precip"  # variable to plot
+# lev = ""   # level
+# contourmin = 0.2  # contour minimum
+# contourmax = 1.2  # contour maximum
+# contourspace = 0.2  # contour spacing
 # hovmoeller(A, lonA, timeA, datestrt, datelast, plotpath, latMin, latMax, spd, source, var, lev,
 #           contourmin, contourmax, contourspace)
 # hovmoeller(A, lonA, timeA, datestrt, datelast, plotpath, latMin, latMax, spd, source, var, lev)
 
-var = "uwnd"  # variable to plot
-lev = "850"   # level
 ds = xr.open_dataset('/data/mgehne/ERAI/MetricsObs/uwnd.erai.850.1p5.2x.1979-2016.nc')
 B = ds.uwnd
 print("extracting time period:")
