5fad30a31549cf686587a9883d1ff3bbd35853c6
mgehne <mgehne@gmail.com>
2020-03-30 14:17:56 -0600

    bug fixes in reading FV3 output

--- examples/Kelvinactivity.py
+++ examples/Kelvinactivity.py
@@ -23,7 +23,7 @@ datelast = '2016-03-29 13:00:00'
 
 
 print("reading ERAI data from file:")
-spd = 1
+spd = 2
 ds = xr.open_dataset('/data/mgehne/ERAI/MetricsObs/precip.erai.sfc.1p0.'+str(spd)+'x.1979-2016.nc')
 A = ds.precip
 print("extracting time period:")
@@ -41,7 +41,7 @@ print(waveactA.min(), waveactA.max())
 
 print("reading observed precipitation data from file:")
 spd = 2
-ds = xr.open_dataset('/data/mgehne/Precip/MetricsObs/precip.trmm.1x.1p0.v7a.fillmiss.comp.1998-2016.nc')
+ds = xr.open_dataset('/data/mgehne/Precip/MetricsObs/precip.trmm.'+str(spd)+'x.1p0.v7a.fillmiss.comp.1998-2016.nc')
 B = ds.precip
 print("extracting time period:")
 B = B.sel(time=slice(datestrt, datelast))
@@ -55,12 +55,12 @@ waveactB = CCEWactivity.waveact(B, wave, eofpath, spd)
 print(waveactB.min(), waveactB.max())
 
 print('reading model forecast from file:')
-spd = 1
+spd = 2
 res1 = 'C128'
 path1 = '/data/mgehne/FV3/replay_exps/C128/ERAI_free-forecast_C128/STREAM_2015103100/MODEL_DATA/SST_INITANOMALY2CLIMO-90DY/ALLDAYS/'
 filebase1 = 'prcp_avg6h_fhr'  #720_C128_180x360.nc
 
-fchrs = np.arange(0, 720, 24)
+fchrs = np.arange(0, 720, 12)
 nfchr = len(fchrs)
 exps = [0, 1, 2]
 explabels = ['trmm', 'erai', res1]
@@ -68,26 +68,21 @@ nexps = len(exps)
 
 fi = 0
 for ff in fchrs:
-    fstr = f"{ff:03d}"
+    fstr = f"{ff:02d}"
     print('Reading fhr='+fstr)
-    ds = xr.open_dataset(path1 + filebase1 + fstr + '.nc')
-    data1 = ds.prate_ave
+    ds = xr.open_dataset(path1 + filebase1 + fstr + '_C128_180x360.nc')
+    data1 = ds.prcp
     data1 = data1.sel(time=slice(datestrt, datelast))
-    ds.close()
-    ds = xr.open_dataset(path2 + filebase2 + fstr + '.nc')
-    data2 = ds.prate_ave
-    data2 = data2.sel(time=slice(datestrt, datelast))
     ds.close()
 
     print('computing activity')
     wact1 = CCEWactivity.waveact(data1, wave, eofpath, spd)
-    wact2 = CCEWactivity.waveact(data2, wave, eofpath, spd)
 
     if fi == 0:
         act = xr.DataArray(0., coords=[fchrs, exps, wact1['time']], dims=['fchrs', 'exps', 'time'])
 
     act[fi, 2, :] = wact1.values
-    act[fi, 3, :] = wact2.values
+    #act[fi, 3, :] = wact2.values
     act[fi, 0, :] = waveactB.values
     act[fi, 1, :] = waveactA.values
 
