6f71df68b3e3dae6345e2805afeb29331d1f8630
mgehne <mgehne@gmail.com>
2020-04-08 08:31:20 -0600

    added read forecast hours to cross_spectra

--- examples/cross_spectra.py
+++ examples/cross_spectra.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import sys
+
 sys.path.append('../../')
 """
 local scripts, if loading from a different directory include that with a '.' between
@@ -20,12 +21,14 @@ Symmetry:    Can be "symm", "asymm" for symmetric or anti-symmetric across the e
 latMin:      Minimum latitude to include.
 latMax:      Maximum latitude to include.
 """
-spd = 2
-nperseg = 90*spd
-segOverLap = -30*spd
-Symmetry = "latband"
+spd = 1
+nperseg = 46 * spd
+segOverLap = -20 * spd
+Symmetry = "symm"
 latMin = -15.
-latMax =  15.
+latMax = 15.
+datestrt = '2015-12-01'  # plot start date, format: yyyy-mm-dd
+datelast = '2016-03-31'  # plot end date, format: yyyy-mm-dd
 
 print("reading data from file:")
 """ 
@@ -35,30 +38,41 @@ x = ds.u
 x = x.sel(lat=slice(latMin,latMax)) 
 x = x.squeeze()
 """
-ds = xr.open_dataset('/data/mgehne/ERAI/MetricsObs/precip.erai.sfc.1p5.2x.1979-2016.nc')
+ds = xr.open_dataset('/data/mgehne/ERAI/MetricsObs/precip.erai.sfc.1p0.1x.1979-2016.nc')
 x = ds.precip
-x = x.sel(lat=slice(latMin,latMax))
+x = x.sel(lat=slice(latMin, latMax))
+x = x.sel(time=slice(datestrt, datelast))
 x = x.squeeze()
-latA = ds.lat.sel(lat=slice(latMin,latMax))
+latA = ds.lat.sel(lat=slice(latMin, latMax))
 ds.close()
 
-ds = xr.open_dataset('/data/mgehne/ERAI/MetricsObs/div.erai.850.1p5.2x.1979-2016.nc')
+ds = xr.open_dataset('/data/mgehne/ERAI/MetricsObs/div.erai.850.1p0.1x.1979-2016.nc')
 y = ds.div
-y = y.sel(lat=slice(latMin,latMax))
+y = y.sel(lat=slice(latMin, latMax))
+y = y.sel(time=slice(datestrt, datelast))
 y = y.squeeze()
-latB = ds.lat.sel(lat=slice(latMin,latMax))
+latB = ds.lat.sel(lat=slice(latMin, latMax))
 
-if any(latA-latB)!=0:
-  print("Latitudes must be the same for both variables! Check latitude ordering.")
+ds = xr.open_dataset('/data/mgehne/Precip/MetricsObs/precip.trmm.1x.1p0.v7a.fillmiss.comp.1998-2016.nc)
+z = ds.precip
+z = z.sel(lat=slice(latMin, latMax))
+z = z.sel(time=slice(datestrt, datelast))
+z = z.squeeze()
+latC = ds.lat.sel(lat=slice(latMin, latMax))
+
+if any(latA - latB) != 0:
+    print("Latitudes must be the same for both variables! Check latitude ordering.")
 
 print("get symmetric/anti-symmetric components:")
-if Symmetry=="symm" or Symmetry=="asymm":
-    X = get_symmasymm(x,latA,Symmetry)
-    Y = get_symmasymm(y,latB,Symmetry)
+if Symmetry == "symm" or Symmetry == "asymm":
+    X = get_symmasymm(x, latA, Symmetry)
+Y = get_symmasymm(y, latB, Symmetry)
+Z = get_symmasymm(z, latC, Symmetry)
 else:
-    X = x
-    Y = y
-    
+X = x
+Y = y
+Z = z
+
 print("compute cross-spectrum:")
 """
 The output from mjo_cross includes:
@@ -78,16 +92,51 @@ dof
 prob
 prob_coh2
 """
-result = mjo_cross(X, Y, nperseg, segOverLap)  
-STC = result['STC'] #, freq, wave, number_of_segments, dof, prob, prob_coh2
-
+result = mjo_cross(X, Y, nperseg, segOverLap)
+STC = result['STC']  # , freq, wave, number_of_segments, dof, prob, prob_coh2
 freq = result['freq']
-freq = freq*spd
+freq = freq * spd
 wnum = result['wave']
-
 # save spectra in netcdf file
-
-fileout = 'SpaceTimeSpectra_'+Symmetry+'_'+str(spd)+'spd'
+fileout = 'SpaceTimeSpectra_ERAI_P_D850_' + Symmetry + '_' + str(spd) + 'spd'
 pathout = '../data/'
-print('saving spectra to file: '+pathout+fileout+'.nc')
-save_Spectra(STC,freq,wnum,fileout,pathout)
+print('saving spectra to file: ' + pathout + fileout + '.nc')
+save_Spectra(STC, freq, wnum, fileout, pathout)
+
+result = mjo_cross(X, Z, nperseg, segOverLap)
+STC = result['STC']  # , freq, wave, number_of_segments, dof, prob, prob_coh2
+freq = result['freq']
+freq = freq * spd
+wnum = result['wave']
+# save spectra in netcdf file
+fileout = 'SpaceTimeSpectra_ERAI_TRMM_P_' + Symmetry + '_' + str(spd) + 'spd'
+pathout = '../data/'
+print('saving spectra to file: ' + pathout + fileout + '.nc')
+save_Spectra(STC, freq, wnum, fileout, pathout)
+
+print("compute cross-spectra at all lead times")
+spd = 1
+res1 = 'C128'
+path1 = '/data/mgehne/FV3/replay_exps/C128/ERAI_free-forecast_C128/STREAM_2015103100/MODEL_DATA/SST_INITANOMALY2CLIMO-90DY/ALLDAYS/'
+filebaseP = 'prcp_avg6h_fhr'  #720_C128_180x360.nc
+filebaseD = 'div850_sh_cdf_f'  #00.nc'
+
+fchrs = np.array(0)
+print(fchrs)
+nfchr = len(fchrs)
+
+for ff in fchrs:
+    fstr = f"{ff:02d}"
+    print('Reading fhr='+fstr)
+    ds = xr.open_dataset(path1 + filebaseP + fstr + '_C128_180x360.nc')
+    prcp = ds.prcp
+    prcp = prcp.sel(time=slice(datestrt, datelast))
+    prcp = prcp.sel(lat=slice(latMin, latMax))
+    prcp = prcp*3600
+    prcp.attrs['units'] = 'mm/d'
+    ds.close()
+    ds = xr.open_dataset(path1 + filebaseP + fstr + '_C128_180x360.nc')
+    div = ds.div
+    div = div.sel(time=slice(datestrt, datelast))
+    div = div.sel(lat=slice(latMin, latMax))
+    ds.close()
