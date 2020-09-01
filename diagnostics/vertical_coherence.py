found bug in frequency average

diagnostics/vertical_coherence.py
@@ -71,7 +71,7 @@ def vertical_coherence_comp(data1, data2, levels, nDayWin, nDaySkip, spd, siglev
     CrossMask = CrossMat * MaskAll
 
     # average coherence across significant values
-    CrossAvg = np.nanmean(CrossMask.sel(freq=slice(0, 1), wave=slice(-20, 20)), axis=(2, 3))
+    CrossAvg = np.nanmean(CrossMask.sel(freq=slice(-1, 1), wave=slice(-20, 20)), axis=(2, 3))
     # recompute phase angles of averaged cross-spectra
     CrossAvg = cross_phase_2d(CrossAvg)
     CrossAvg = xr.DataArray(CrossAvg, dims=['level', 'cross'], coords={'level': levels, 'cross': np.arange(0, 16, 1)})