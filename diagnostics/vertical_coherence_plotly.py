2f6d56eaf6cd04239b47b5e2646baa88e92d6201
mgehne <mgehne@gmail.com>
2020-09-16 08:46:41 -0600

    new version of coherence plot, with subplots for phase angles

--- diagnostics/vertical_coherence_plotly.py
+++ diagnostics/vertical_coherence_plotly.py
@@ -5,6 +5,7 @@ plot full spectra
 
 import numpy as np
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from kaleido.scopes.plotly import PlotlyScope
 
 def get_latstring(lats, latn):
@@ -51,28 +52,67 @@ def plot_vertcoh(coh, px, py, levels, labels, titlestr, plotname, plotpath, lats
 
     # compute phase angle (in degrees) from x-y components.
     angcnst = 1.
-    angle = np.arctan2(angcnst*px, py)*180/np.pi+90
+    angle = np.arctan2(angcnst * px, py) * 180 / np.pi
+    print(np.min(angle), np.max(angle))
 
     # latitude string for plot title
     latstring = get_latstring(lats, latn)
 
     # set up plotname for saving
     plttype = "png"
-    plotname = plotpath + plotname+"." + plttype
+    plotname = plotpath + plotname + "." + plttype
 
     # plot
     nlines = len(labels)
+    colors = ['firebrick', 'black', 'orange', 'dodgerblue', 'seagreen']
     scope = PlotlyScope()
-    fig = go.Figure()
+    # fig = go.Figure()
+    fig = make_subplots(rows=1, cols=2, shared_yaxes=True, horizontal_spacing=0.04)
+
     for ll in np.arange(0, nlines):
         fig.add_trace(go.Scatter(x=coh[ll, :], y=levels,
-                             mode='lines',
-                             name=labels[ll]))
+                                 mode='lines',
+                                 name=labels[ll],
+                                 line=dict(color=colors[ll], width=2)),
+                      row=1, col=1)
+        fig.add_trace(go.Scatter(x=angle[ll, :], y=levels,
+                                 mode='lines',
+                                 showlegend=False,
+                                 line=dict(color=colors[ll], width=2)),
+                      row=1, col=2)
 
-    fig.update_layout(title=titlestr+' '+latstring)
+    fig.add_annotation(
+        x=-90,
+        y=50,
+        xref="x2",
+        yref="y2",
+        text="precip lags",
+        showarrow=False,
+        bgcolor="white",
+        opacity=0.8
+    )
+    fig.add_annotation(
+        x=90,
+        y=50,
+        xref="x2",
+        yref="y2",
+        text="precip leads",
+        showarrow=False,
+        bgcolor="white",
+        opacity=0.8
+    )
 
-    fig.update_xaxes(ticks="", tick0=0, dtick=0.1, title_text='coh^2')
-    fig.update_yaxes(ticks="", tick0=0, dtick=50, title_text='hPa', autorange="reversed")
+    fig.update_layout(title=titlestr + ' ' + latstring, width=900, height=600,
+                      legend=dict(yanchor="bottom", y=0.01, xanchor="right", x=0.48)
+                      )
+
+    fig.update_xaxes(title_text='coh^2', range=[0, 0.25], row=1, col=1)
+    fig.update_xaxes(title_text='phase angle', range=[-180, 180], dtick=90, row=1, col=2)
+    fig.update_yaxes(range=[100, 1000], dtick=100, title_text='hPa', autorange="reversed", row=1, col=1)
+    fig.update_yaxes(range=[100, 1000], dtick=100, autorange="reversed", row=1, col=2)
 
     with open(plotname, "wb") as f:
         f.write(scope.transform(fig, format=plttype))
+
+    return
+
